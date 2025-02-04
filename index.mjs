7f397ae3942320fe984826a10f3bcf979be60302
Takuto Wada <takuto.wada@gmail.com>
2025-02-04 10:01:57 +0900

    style: apply neostandard

--- index.mjs
+++ index.mjs
@@ -2,11 +2,11 @@ import { Transform } from 'node:stream';
 
 const customReporter = new Transform({
   writableObjectMode: true,
-  transform(event, encoding, callback) {
+  transform (event, encoding, callback) {
     switch (event.type) {
       case 'test:start': {
         const suiteMarker = event.data.nesting === 0 ? '###' : '-';
-        callback(null, `${"  ".repeat(event.data.nesting)}${suiteMarker} ${event.data.name}\n`);
+        callback(null, `${'  '.repeat(event.data.nesting)}${suiteMarker} ${event.data.name}\n`);
         break;
       }
       default: {
